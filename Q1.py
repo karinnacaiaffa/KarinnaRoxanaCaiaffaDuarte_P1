de38864e3424c6b3b411704c3c227fede8e9157d
karinnacaiaffa <56059592+karinnacaiaffa@users.noreply.github.com>
2019-10-02 01:17:29 -0300

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,22 @@
+########### Questao 1 ###########
+
+a = int( input(" Entre com a!=0: "))
+b = int( input(" Entre com b: "))
+c = int( input(" Entre com c: "))
+
+def raizes (a, b, c):
+
+    delta = (b ** 2) - (4 * a * c)
+
+    if (delta >= 0):
+        raiz1 = (-b + ((delta) ** (1/2)))/(2 * a)
+        raiz2 = (-b - ((delta) ** (1/2)))/(2 * a)
+        print ("Raiz 1= ", raiz1, "e raiz 2= ", raiz2)
+        return 0
+
+    else:
+        real= -b / (2 * a)
+        imaginaria= ((delta) ** (1/2))/(2 * a)
+        print ("Parte real=", real," e parte imaginaria=", imaginaria)
+        return 1
+
